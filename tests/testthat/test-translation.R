e91db540b55c543dd0a5d6136c1ed9b18b645805
minhao <lingminhao31@gmail.com>
2022-05-13 00:13:38 +0800

    added unit test

--- tests/testthat/test-translation.R
+++ tests/testthat/test-translation.R
@@ -1,3 +1,7 @@
 test_that("translation() converts RNA to Amino acid", {
   expect_equal(translation("AUGGCCAUGGCGCCCAGAACUGAGAUCAAUAGUACCCGUAUUAACGGGUGA"), "MAMAPRTEINSTRING")
 })
+
+test_that("translation() terminates immediately for stop codon", {
+  expect_equal(translation("AUGUGAAUGGCGCCCAGAACUGAGAUCAAUAGUACCCGUAUUAACGGGUGA"), "M")
+})
