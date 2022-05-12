cd9e26021be9aef0141700a042fdb5ea507c0d37
minhao <lingminhao31@gmail.com>
2022-05-12 22:55:40 +0800

    added codon table

---
+++ R/codon_table.R
@@ -0,0 +1,13 @@
+#' Codon table.
+#'
+#' A dataset containing the maps between codon and its respective amino acid
+#' representation.
+#'
+#' @format A data frame with 64 rows and 2 variables:
+#' \describe{
+#'   \item{codon}{codon, in 3-mers}
+#'   \item{amino}{amino acid code, in 1-mer}
+#'   ...
+#' }
+#' @source \url{https://github.com/lingminhao/Project-Rosalind/blob/main/Bioinformatics%20Textbook%20Track/Chapter%204/CodonTable.txt}
+"codon_table"
