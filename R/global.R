c090141707c4f90b574556261499e2b1c7ef6bd3
minhao <lingminhao31@gmail.com>
2022-05-13 14:44:12 +0800

    changed codon_table from binary to txt

--- R/global.R
+++ R/global.R
@@ -1,1 +1,8 @@
+readfile <- read.csv(system.file("extdata", "codon_table.txt",
+                                 package = "aminoamino", mustWork = TRUE),
+                     header = FALSE,sep = " ")
+codon_table <- data.frame(readfile[,2])
+colnames(codon_table) <- "amino"
+rownames(codon_table) <- readfile[,1]
+
 utils::globalVariables("codon_table")
