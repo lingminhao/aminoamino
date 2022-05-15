c8035a82e5f21fc9738cace6bbc5947ad89b6a41
Ling Min Hao <lingminhao@Lings-MacBook-Air.local>
2022-05-15 16:32:01 +0800

    edited code for codon_table

--- R/global.R
+++ R/global.R
@@ -1,8 +1,5 @@
-readfile <- read.csv(system.file("extdata", "codon_table.txt",
+codon_table <- read.csv(system.file("extdata", "codon_table.txt",
                                  package = "aminoamino", mustWork = TRUE),
-                     header = FALSE,sep = " ")
-codon_table <- data.frame(readfile[,2])
-colnames(codon_table) <- "amino"
-rownames(codon_table) <- readfile[,1]
+                     header = FALSE,sep = " ", row.names = 1, col.names = c('codon', 'amino'))
 
 utils::globalVariables("codon_table")
